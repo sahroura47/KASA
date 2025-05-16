3b3d4b3098d0edd0d4033e24e883cd3668925a6b
sahroura47 <166107845+sahroura47@users.noreply.github.com>
2025-05-16 11:30:34 +0200

    add condition to prevent arrow display if there is one picture, ajustments in css

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -14,19 +14,20 @@ function ImageCarousel({ pictures }) {
   if (!pictures || pictures.length === 0) {
     return <div> Aucune image à afficher</div>;
   }
+  const showArrows= pictures.length >1;
   return (
     <div className="carouselContainer">
-      <button className="arrowLeft" onClick={handlePrev}>
+      { showArrows && (<button className="arrowLeft" onClick={handlePrev}>
         <img src={arrowLeft}  alt="précédent" />
-      </button>
+      </button> )}
       <img
         src={pictures[currentImageIndex]}
         alt={"picture" + currentImageIndex + 1}
         className="carouselImage"
       />
-      <button className="arrowRight" onClick={handleNext}>
+      { showArrows && (<button className="arrowRight" onClick={handleNext}>
         <img src={arrowRight} alt="suivant" />
-      </button>
+      </button>)}
     </div>
   );
 }
