346f7f61099f3ce8ae80382b46bfec4178649f22
sahroura47 <166107845+sahroura47@users.noreply.github.com>
2025-05-26 16:15:27 +0200

    cardsContainer test adjustment

--- src/components/Cards-container.test.jsx
+++ src/components/Cards-container.test.jsx
@@ -1,15 +1,8 @@
 import { screen, render, waitFor } from "@testing-library/react";
 import CardsContainer from "./Cards-container";
 import { afterAll, afterEach, beforeAll, describe, expect } from "vitest";
-import { server, setupServer } from '../mocks/serverSetup';
-import { http } from "msw";
+import { server } from '../mocks/serverSetup';
 import { MemoryRouter } from "react-router-dom";
-
-/* des données simulées*/
-const fakeCards = [
-  { id: "1", title: "Appartement Paris", pictures: ["image1.jpg"] },
-  { id: "2", title: "Villa Nantes", pictures: ["image2.jpg"] },
-];
 
  /* on démarre et arrête le serveur */
  beforeAll(()=> server.listen());
@@ -18,12 +11,6 @@ const fakeCards = [
 
  describe('cardsContainer component', ()=>{
     test('cards display', async ()=>{
-        server.use(
-            /*on simule on serveur*/ 
-
-    http.get('http://localhost:8080/api/properties',(req,res)=>{
-        return res.json(fakeCards);
-    }));
         render(
             <MemoryRouter>
                 <CardsContainer />
@@ -32,10 +19,10 @@ const fakeCards = [
    /* on vérifie si les titres sont affichés */
    await waitFor (()=>{
     expect(screen.getByText(/Appartement Paris/i)).toBeInTheDocument();
-    expect(screen.getByText(/Villa Nantes/i)).toBeInTheDocument();
+    expect(screen.getByText(/Maison Nantes/i)).toBeInTheDocument();
    });
    /* on vérifie si les images sont présentes */ 
-   expect(screen.getByAltText(/Appartement Paris/i)).toHaveAttribute('src', 'image1.jpg');
-   expect(screen.getByAltText(/Villa Nantes/i)).toHaveAttribute('src', 'image2.jpg');
+   expect(screen.getByAltText(/Appartement Paris/i)).toHaveAttribute('src', 'image2.jpg');
+   expect(screen.getByAltText(/Maison Nantes/i)).toHaveAttribute('src', 'image1.jpg');
     });
  });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,12 +1,11 @@
-import { http, rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('http://localhost:8080/api/properties', (req, res, ctx) => {
-    return res(
-      ctx.json([
-        { id: '1', title: 'Maison', pictures: ['url1.jpg'] },
-        { id: '2', title: 'Appartement', pictures: ['url2.jpg'] },
-      ])
-    );
-  }),
+  http.get('http://localhost:8080/api/properties', () => {
+    return  HttpResponse.json([
+        { id: '1', title: 'Maison Nantes', pictures: ['image1.jpg'] },
+        { id: '2', title: 'Appartement Paris', pictures: ['image2.jpg'] },
+      ], {status:200});
+    }),
+
 ];
